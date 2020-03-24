0030e60602274771adfcf463a37c9eb39d0907e5
Aaron Noyes <noyesa@myumanitoba.ca>
2020-03-24 17:24:41 -0500

    Refactored setup to one script

--- setup_serv.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-#configure tun0
-#IP assigned from first command line arg
-sudo ip addr add $1/24 dev tun0
-sudo ifconfig tun0 up
-
-#route traffic to server through tun0
-route add -net $2 netmask 255.255.255.0 dev tun0
