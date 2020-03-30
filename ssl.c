5fb89f6fe5491cf8b681c5f763366e7a3dbeae8d
Aaron Noyes <noyesa@myumanitoba.ca>
2020-03-30 10:41:37 -0500

    Fixed typo

--- ssl.c
+++ ssl.c
@@ -31,7 +31,7 @@ SSL_CTX *ssl_init_ctx(char *cafile , char *keyfile, char *password, int server) 
         method = SSLv23_method();
     }
     else { 
-        method = SSLv3_client_method();
+        method = SSLv23_client_method();
     }
 
     if (!method) {
