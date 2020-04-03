dab96a8396cd37f796c1c29a0c8292a9964742d7
Aaron Noyes <noyesa@myumanitoba.ca>
2020-04-02 20:10:34 -0500

    Fork for clients

--- server.c
+++ server.c
@@ -59,7 +59,8 @@ char *progname;
 int main(int argc, char *argv[]) {
   
   int tap_fd;
-  char if_name[IFNAMSIZ] = "tun0";
+  char if_name[IFNAMSIZ] = "";
+  char if_name_format = "tun%d";
   struct sockaddr_in client_tcp, client_udp;
   char tun_ip[IP_AD_LEN] = "";
   char cli_vpn_ip[IP_AD_LEN] = "";
@@ -71,26 +72,14 @@ int main(int argc, char *argv[]) {
   unsigned char iv[AES_IV_SIZE + 1];
   SSL_CTX *ctx;
   SSL *ssl;
+  int num_cli = 0;
 
   progname = argv[0];
   
   parse_args(argc, argv, "i:hdt:", NULL, tun_ip);
 
-  //initialize tun interface
-  if ( (tap_fd = tun_alloc(if_name, IFF_TUN | IFF_NO_PI)) < 0 ) {
-    my_err("Error connecting to tun/tap interface %s!\n", if_name);
-    exit(1);
-  }
-  do_debug("Successfully connected to interface %s\n", if_name);
-
-  tunsock = tun_config(tun_ip, if_name);
-  if (!tunsock) {
-    exit(1);
-  }
-
   //open a a stream socket for SSL, and a datagram socket for tunnel
   serv_sock = get_sock(port, SOCK_STREAM, 0);
-  dg_sock = get_sock(ANYPORT, SOCK_DGRAM, IPPROTO_UDP);
   
   remotelen = sizeof(client_tcp);
   memset(&client_tcp, 0, remotelen);
@@ -101,69 +90,91 @@ int main(int argc, char *argv[]) {
       exit(1);
   }
 
-  //create a new socket from the incoming connection
-  //also loads remote info for conencted client
-  if ((s_sock = accept(serv_sock, (struct sockaddr*)&client_tcp, &remotelen)) < 0){
-      perror("accept()");
-      exit(1);
+  while(1) {
+    //create a new socket from the incoming connection
+    //also loads remote info for conencted client
+    if ((s_sock = accept(serv_sock, (struct sockaddr*)&client_tcp, &remotelen)) < 0){
+        perror("accept()");
+        exit(1);
+    }
+    do_debug("Received tcp connection\n");
+
+    if (fork() == 0) {
+
+      snprintf(if_name, IFNAMSIZ-4, if_name_format, num_cli++);
+
+      //establish SSL connection
+      ctx = ssl_init_ctx(CA_FILE, KEYFILE, SERV_KEY_PASS, CERTFILE, 1);
+      if (!ctx) {
+        do_debug("SSL context init failed\n");
+        ERR_print_errors_fp(stderr);
+        abort();
+      }
+      do_debug("SSL context init complete\n");
+
+      ssl = ssl_handsh(s_sock, ctx, 1);
+      if (!ssl) {
+        do_debug("SSL handshake failed\n");
+        ERR_print_errors_fp(stderr);
+        abort();
+      }
+      do_debug("SSL handshake complete\n");
+
+      //generate key and send to client over SSL connection
+      getrandom(key, AES_KEYSIZE, 0);
+      *(key+AES_KEYSIZE) = '\0';
+      SSL_write(ssl, key, AES_KEYSIZE + 1);
+      do_debug("Sent session key\n");
+
+      //generate iv and send to client over SSL connection
+      getrandom(iv, AES_KEYSIZE, 0);
+      *(iv+AES_IV_SIZE) = '\0';
+      SSL_write(ssl, iv, AES_IV_SIZE + 1);
+      do_debug("Sent session key\n");
+
+      //send VPN ip to server, get client's vpn IP
+      SSL_write(ssl, tun_ip, IP_AD_LEN);
+      SSL_read(ssl, cli_vpn_ip, IP_AD_LEN);
+      add_n_route(cli_vpn_ip, if_name);
+      do_debug("Added client's VPN IP to routing table\n");
+
+      //location of client's udp port
+      memset(&client_udp, 0, sizeof(client_udp));
+      client_udp.sin_family = AF_INET;
+      client_udp.sin_addr.s_addr = client_tcp.sin_addr.s_addr;
+      client_udp.sin_port = htons(port + 1);
+
+      //initialize tun interface
+      if ( (tap_fd = tun_alloc(if_name, IFF_TUN | IFF_NO_PI)) < 0 ) {
+        my_err("Error connecting to tun/tap interface %s!\n", if_name);
+        exit(1);
+      }
+      do_debug("Successfully connected to interface %s\n", if_name);
+
+      tunsock = tun_config(tun_ip, if_name);
+      if (!tunsock) {
+        exit(1);
+      }
+
+      dg_sock = get_sock(ANYPORT, SOCK_DGRAM, IPPROTO_UDP);
+
+      //send buffer to client so that it gets our datagram socket
+      if (sendto(dg_sock, buffer, BUFSIZE, 0, (struct sockaddr*)&client_udp, sizeof(client_udp)) < 0) {
+        perror("sendto()");
+        exit(1);
+      }
+      do_debug("Sent blank buffer to connect to client's UDP port\n");
+
+      do_tun_loop(tap_fd, dg_sock, s_sock, ssl, client_udp, key, iv);
+
+      close(dg_sock);
+      close(s_sock);
+      close(tunsock);
+      close(tap_fd);
+    }
   }
-  do_debug("Received tcp connection\n");
 
-  //establish SSL connection
-  ctx = ssl_init_ctx(CA_FILE, KEYFILE, SERV_KEY_PASS, CERTFILE, 1);
-  if (!ctx) {
-    do_debug("SSL context init failed\n");
-    ERR_print_errors_fp(stderr);
-    abort();
-  }
-  do_debug("SSL context init complete\n");
-
-  ssl = ssl_handsh(s_sock, ctx, 1);
-  if (!ssl) {
-    do_debug("SSL handshake failed\n");
-    ERR_print_errors_fp(stderr);
-    abort();
-  }
-  do_debug("SSL handshake complete\n");
-
-  //generate key and send to client over SSL connection
-  getrandom(key, AES_KEYSIZE, 0);
-  *(key+AES_KEYSIZE) = '\0';
-  SSL_write(ssl, key, AES_KEYSIZE + 1);
-  do_debug("Sent session key\n");
-
-  //generate iv and send to client over SSL connection
-  getrandom(iv, AES_KEYSIZE, 0);
-  *(iv+AES_IV_SIZE) = '\0';
-  SSL_write(ssl, iv, AES_IV_SIZE + 1);
-  do_debug("Sent session key\n");
-
-  //send VPN ip to server, get client's vpn IP
-  SSL_write(ssl, tun_ip, IP_AD_LEN);
-  SSL_read(ssl, cli_vpn_ip, IP_AD_LEN);
-  add_n_route(cli_vpn_ip, if_name);
-  do_debug("Added client's VPN IP to routing table\n");
-
-  //location of client's udp port
-  memset(&client_udp, 0, sizeof(client_udp));
-  client_udp.sin_family = AF_INET;
-  client_udp.sin_addr.s_addr = client_tcp.sin_addr.s_addr;
-  client_udp.sin_port = htons(port + 1);
-
-  //send buffer to client so that it gets our datagram socket
-  if (sendto(dg_sock, buffer, BUFSIZE, 0, (struct sockaddr*)&client_udp, sizeof(client_udp)) < 0) {
-    perror("sendto()");
-    exit(1);
-  }
-  do_debug("Sent blank buffer to connect to client's UDP port\n");
-
-  do_tun_loop(tap_fd, dg_sock, s_sock, ssl, client_udp, key, iv);
-
-  close(dg_sock);
-  close(s_sock);
-  close(tunsock);
   close(serv_sock);
-  close(tap_fd);
   
   return(0);
 }
